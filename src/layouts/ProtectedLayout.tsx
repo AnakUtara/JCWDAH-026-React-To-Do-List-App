a4e08224a68bf66713ec6dc77b597949f563bb09
AnakUtara <141168468+AnakUtara@users.noreply.github.com>
2026-06-11 23:37:40 +0700

    feat: implement isAuthenticating check in AuthContext.tsx to trigger loading screen on route guards in each layouts

--- src/layouts/ProtectedLayout.tsx
+++ src/layouts/ProtectedLayout.tsx
@@ -1,8 +1,13 @@
 import { useAuth } from "@/context/AuthContext";
+import LoadingPage from "@/pages/LoadingPage";
 import { Navigate, Outlet } from "react-router";
 
 const ProtectedLayout = () => {
-	const { user } = useAuth();
+	const { user, isAuthenticating } = useAuth();
+
+	if (isAuthenticating) {
+		return <LoadingPage />;
+	}
 
 	if (!user) {
 		return <Navigate to="/sign-in" replace />;
--- src/layouts/PublicLayout.tsx
+++ src/layouts/PublicLayout.tsx
@@ -1,8 +1,13 @@
 import { useAuth } from "@/context/AuthContext";
+import LoadingPage from "@/pages/LoadingPage";
 import { Navigate, Outlet } from "react-router";
 
 const PublicLayout = () => {
-	const { user } = useAuth();
+	const { user, isAuthenticating } = useAuth();
+
+	if (isAuthenticating) {
+		return <LoadingPage />;
+	}
 
 	if (user) {
 		return <Navigate to="/" replace />;
